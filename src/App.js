0f5d9fe8ec1ff8a1770453a2600dd0bff8321a89
mjggel <luismiguelvieira015@gmail.com>
2022-09-08 15:00:53 -0300

    Iniciando projeto Trivia React Redux

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
       </header>
     </div>
   );
