686bbf5c24eda4992a10d897c81fbc00389464b0
mjggel <luismiguelvieira015@gmail.com>
2022-09-08 15:44:31 -0300

    Requisito 1

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
 import user from './user';
 
-const rootReducer = combineReducers(
-  {
-    user,
-  },
-);
+const rootReducer = combineReducers({ user });
 
 export default rootReducer;
