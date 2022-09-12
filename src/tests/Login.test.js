3bf5354bfdb76f611bf5ea6fa94570b852ad26fc
Guyddo <guyddogl@gmail.com>
2022-09-12 17:40:36 -0300

    Requisitos 18 Login.test

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -3,6 +3,7 @@ import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import { getTriviaToken, getTriviaQuestions } from '../services/fetchapi';
+import { questionsResponse } from './helpers/mocks';
 import App from '../App';
 
 const EMAIL_INPUT_TEST_ID = 'input-gravatar-email';
@@ -68,86 +69,26 @@ describe('Testa se a página de login renderiza corretamente', () => {
     expect(global.fetch).toHaveBeenCalledWith('https://opentdb.com/api_token.php?command=request');
   });
 
-  // test('Verifica se o usuário consegue iniciar o jogo', async () => {
-  //   const STATE = {
+  test('Verifica se o usuário consegue iniciar o jogo', async () => {
 
-  //     reducerQuestions: {
-  //       response_code: 0,
-  //       results: [
-  //         {
-  //           category: 'Entertainment: Film',
-  //           type: 'multiple',
-  //           difficulty: 'medium',
-  //           question: 'In the 1994 movie &quot;Speed&quot;, what is the minimum speed the bus must go to prevent to bomb from exploding?',
-  //           correct_answer: '50 mph',
-  //           incorrect_answers: [
-  //             '60 mph',
-  //             '40 mph',
-  //             '70 mph'
-  //           ]
-  //         },
-  //         {
-  //           category: 'Science: Computers',
-  //           type: 'boolean',
-  //           difficulty: 'medium',
-  //           question: 'AMD created the first consumer 64-bit processor.',
-  //           correct_answer: 'True',
-  //           incorrect_answers: [
-  //             'False'
-  //           ]
-  //         },
-  //         {
-  //           category: 'History',
-  //           type: 'multiple',
-  //           difficulty: 'medium',
-  //           question: 'What was the total length of the Titanic?',
-  //           correct_answer: '882 ft | 268.8 m',
-  //           incorrect_answers: [
-  //             '759 ft | 231.3 m',
-  //             '1042 ft | 317.6 m',
-  //             '825 ft | 251.5 m'
-  //           ]
-  //         },
-  //         {
-  //           category: 'Entertainment: Music',
-  //           type: 'multiple',
-  //           difficulty: 'medium',
-  //           question: 'Which of these bands was a featuring artist in Compton rapper Kendrick Lamar&#039;s 2017 album, &quot;DAMN.&quot;?',
-  //           correct_answer: 'U2',
-  //           incorrect_answers: [
-  //             'Radiohead',
-  //             'Coldplay',
-  //             'Bon Jovi'
-  //           ]
-  //         },
-  //         {
-  //           category: 'Entertainment: Video Games',
-  //           type: 'boolean',
-  //           difficulty: 'medium',
-  //           question: 'The character that would eventually become Dr. Eggman was considered for the role of Sega&#039;s new flagship mascot before Sonic was.',
-  //           correct_answer: 'True',
-  //           incorrect_answers: [
-  //             'False'
-  //           ]
-  //         }
-  //       ]
-  //     }
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(questionsResponse),
+    });
 
-  //   }
+    const { history } = renderWithRouterAndRedux(<App />);
+    const inputEmail = screen.getByTestId(EMAIL_INPUT_TEST_ID);
+    const inputName = screen.getByTestId(NAME_INPUT_TEST_ID);
+    const buttonPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
 
-  //   const { history } = renderWithRouterAndRedux(<App />, STATE);
-  //   const inputEmail = screen.getByTestId(EMAIL_INPUT_TEST_ID);
-  //   const inputName = screen.getByTestId(NAME_INPUT_TEST_ID);
-  //   const buttonPlay = screen.getByTestId(BUTTON_PLAY_TEST_ID);
+    userEvent.type(inputEmail, 'teste@email.com');
+    userEvent.type(inputName, 'name');
 
-  //   userEvent.type(inputEmail, 'teste@email.com');
-  //   userEvent.type(inputName, 'name');
+    expect(buttonPlay).toBeEnabled();
 
-  //   expect(buttonPlay).toBeEnabled();
+    userEvent.click(buttonPlay);
 
-  //   userEvent.click(buttonPlay);
+    await waitFor(() => { expect(history.location.pathname).toBe('/game') });
 
-  //   expect(history.location.pathname).toBe('/game');
-
-  // });
+  });
 });
