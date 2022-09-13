37e001ff2d6971f525a8279d45dd7839529fce88
Guilherme Garcia <garciaguig@gmail.com>
2022-09-13 14:56:10 -0300

    Conclusão do Req 17

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
-import { getTriviaToken, getTriviaQuestions } from '../services/fetchapi';
 import { questionsResponse } from './helpers/mocks';
 import App from '../App';
 
